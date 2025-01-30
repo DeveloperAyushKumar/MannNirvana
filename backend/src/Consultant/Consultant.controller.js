b3f0d8e1c3be7ef42f77ed8eb55208b100d75fad
DeveloperAyushKumar <ayush30000000000@gmail.com>
2025-01-30 17:30:28 +0530

    consultant working again

--- backend/src/Consultant/Consultant.controller.js
+++ backend/src/Consultant/Consultant.controller.js
@@ -2,7 +2,7 @@ import Consultant from "./Consultant.model.js";
 const getAllConsultants = async (req, res) => {
     try {
         const consultants = await Consultant.find();
-        res.status(200).json({ consultants });
+        res.status(200).json( consultants );
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -13,7 +13,7 @@ const getConsultant = async (req, res) => {
         if (!consultant) {
             return res.status(404).json({ message: "Consultant not found" });
         }
-        res.status(200).json({ consultant });
+        res.status(200).json( consultant );
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -22,7 +22,7 @@ const createConsultant = async (req, res) => {
     try {
         const consultant = new Consultant(req.body);
         await consultant.save();
-        res.status(201).json({ consultant });
+        res.status(201).json( consultant );
     } catch (error) {
         res.status(400).json({ message: error.message });
     }
