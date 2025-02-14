241e43999fe2b54b3d2e0993a052ecfb573d35ce
Ayush <ag8350961@gmail.com>
2025-02-14 22:26:11 +0530

    track your emoiton added

--- backend/src/chatbot/bot.route.js
+++ backend/src/chatbot/bot.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import getResponse from "./bot.controller.js";
+import { getResponse, getAllemotions } from "./bot.controller.js";
 
 const router =express.Router();
 
 // fetching all emotions
 router.route("/generate-response/").post(getResponse);
+router.route("/get-emotion/").post(getAllemotions);
 
 export default router;
