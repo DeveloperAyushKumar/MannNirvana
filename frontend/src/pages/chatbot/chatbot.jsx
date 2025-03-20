d9015f3b2f1543c731d5feff94b226c4f83f0a62
Ayush <ag8350961@gmail.com>
2025-03-20 20:24:53 +0530

    changes

--- frontend/src/pages/chatbot/chatbot.jsx
+++ frontend/src/pages/chatbot/chatbot.jsx
@@ -6,6 +6,7 @@ import { RxCross2 } from "react-icons/rx";
 import { BsSoundwave } from "react-icons/bs";
 import { ClipLoader } from 'react-spinners';
 import { FaPaperPlane } from "react-icons/fa";
+import { FaMicrophoneSlash } from "react-icons/fa";
 import axios from 'axios';
 
 const BackendURL = import.meta.env.VITE_BACKEND_URL;
@@ -18,46 +19,14 @@ const Bot = () => {
     const [recognition, setRecognition] = useState(null);
     const isRecording = useRef(false);
     const [speech, setSpeech] = useState(false);
-
-    useEffect(() => {
-        // Initialize SpeechRecognition
-        let footer = document.querySelector("#footer");
-        if(footer){
-            footer.style.display = "none";
-        }
-
-        if ("webkitSpeechRecognition" in window || "SpeechRecognition" in window) {
-          const SpeechRecognitionAPI = window.SpeechRecognition || window.webkitSpeechRecognition;
-          const recognitionInstance = new SpeechRecognitionAPI();
-          recognitionInstance.continuous = true;
-          recognitionInstance.interimResults = true;
-          recognitionInstance.lang = "en-US";
-    
-          recognitionInstance.onresult = (event) => {
-            let finalTranscript = "";
-            for (let i = 0; i < event.results.length; i++) {
-              finalTranscript += event.results[i][0].transcript + " ";
-            }
-            setTranscript(finalTranscript);
-          };
-    
-          setRecognition(recognitionInstance);
-        } else {
-          console.warn("Speech Recognition API is not supported in this browser.");
-        }
-
-        return () => {
-            let footer = document.querySelector("#footer");
-            if (footer) {
-                footer.style.display = "block";
-            }
-        }
-    }, []);
+    const voiceChat = useRef(false);
+    const timeoutRef = useRef(null);
+    const indexRef = useRef(0);
 
     const record = () => {
         if (recognition && !isRecording.current) {
             setTranscript(""); // Clear previous transcript
-            setSpeech(!speech);
+            setSpeech(true);
             recognition.start();
             isRecording.current = true;
         }
@@ -66,7 +35,7 @@ const Bot = () => {
     const stop = () => {
         if (recognition && isRecording.current) {
           recognition.stop();
-          setSpeech(!speech);
+          setSpeech(false);
           isRecording.current = false;
         }
     };
@@ -103,10 +72,126 @@ const Bot = () => {
         }
     };
 
+    const handleVoiceQuery = async (transcript) => {
+        try {            
+            const message = transcript.trim();
+            console.log(message);
+
+            if (!message) {
+                console.log("Please enter a valid query");
+                return;
+            }
+
+            if (!user) {
+                console.log("Please login to chat with your dost");
+                return;
+            }
+
+            setLoading(true);
+
+            const res = await axios.post(`${BackendURL}/chatbot/generate-response/`, {
+                text: message,
+                user_id: user._id,
+            });
+
+            speak(res.data.response.slice(14));
+            indexRef.current += res.data.response.slice(14).length;
+            return;
+        } catch (error) {
+            console.error("Error:", error);
+            console.log("Error in fetching response");
+            return;
+        } finally {
+            setLoading(false);
+        }
+    };    
+
+    const speak = (text) => {
+        return new Promise((resolve) => {
+          const synth = window.speechSynthesis;
+          const utterance = new SpeechSynthesisUtterance(text);
+          utterance.onend = () => resolve(true);
+          synth.speak(utterance);
+        });
+    };
+
+    useEffect(() => {
+        // Initialize SpeechRecognition
+        let footer = document.querySelector("#footer");
+        if(footer){
+            footer.style.display = "none";
+        }
+
+        if ("webkitSpeechRecognition" in window || "SpeechRecognition" in window) {
+          const SpeechRecognitionAPI = window.SpeechRecognition || window.webkitSpeechRecognition;
+          const recognitionInstance = new SpeechRecognitionAPI();
+          recognitionInstance.continuous = true;
+          recognitionInstance.interimResults = true;
+          recognitionInstance.lang = "en-US";
+    
+          recognitionInstance.onresult = (event) => {
+            let finalTranscript = "";
+            for (let i = indexRef.current; i < event.results.length; i++) {
+              finalTranscript += event.results[i][0].transcript + " ";
+            }
+
+            if(voiceChat.current){
+                clearTimeout(timeoutRef.current);
+                timeoutRef.current = setTimeout(()=>{
+                    handleVoiceQuery(finalTranscript);
+                    indexRef.current = event.results.length;
+                }, 5000);
+            }
+            else{
+                setTranscript(finalTranscript);
+            }
+          };
+    
+          setRecognition(recognitionInstance);
+        } else {
+          console.warn("Speech Recognition API is not supported in this browser.");
+        }
+
+        return () => {
+            let footer = document.querySelector("#footer");
+            if (footer) {
+                footer.style.display = "block";
+            }
+
+            clearTimeout(timeoutRef.current);
+            stop();
+        }
+    }, [voiceChat]);
+
     return (
         <div className="flex flex-col h-[85vh] items-center">
             <ToastContainer />
 
+            {voiceChat.current? 
+            <div className="w-[90vw] h-full flex flex-col p-4">
+                <div className='flex-2 h-[90vh] flex items-center justify-center'>
+                    <BsSoundwave size={120} /> 
+                </div>
+
+                <div className='flex-1 flex w-full gap-36 items-center justify-center'>
+                    <div className='border p-4 rounded-full'>
+                        {speech? 
+                        <FaMicrophone size={32} className="text-white" onClick={stop} />
+                        :
+                        <FaMicrophoneSlash size={32} className="text-red-500 hover:text-red-600" onClick={record} />}
+                    </div>
+
+                    <div className='border p-4 rounded-full'>
+                        <RxCross2 size={32} className="text-gray-500 hover:text-gray-600" onClick={()=>{
+                            voiceChat.current = false;
+                            indexRef.current = 0;
+                            setTranscript("");
+                            stop();
+                        }} />
+                    </div>
+                </div>
+            </div>
+            :
             <div className="w-[90vw] h-full flex flex-col p-4">
 
                 <div className="flex-1 overflow-y-auto p-4 rounded-lg">
@@ -148,10 +233,17 @@ const Bot = () => {
                             <FaPaperPlane size={24} />
                         </button>
                         : 
-                        <BsSoundwave size={30} /> 
+                        <BsSoundwave size={30} onClick={()=>{
+                            let greet = "Hello! How can I help you?";
+                            voiceChat.current = true;
+                            setSpeech(true);
+                            speak(greet);
+                            record();
+                            indexRef.current+= greet.length;
+                        }} /> 
                     )}
                 </form>
-            </div>
+            </div>}
         </div>
     );
 };
